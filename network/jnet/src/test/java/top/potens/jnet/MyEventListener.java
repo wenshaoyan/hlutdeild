01ba43ef4f75a29254fff1e34650dde005f16365
wenshao <wenshao314106@163.com>
2018-06-24 21:47:59 +0800

    增加rpc event

---
+++ network/jnet/src/test/java/top/potens/jnet/MyEventListener.java
@@ -0,0 +1,24 @@
+package top.potens.jnet;
+
+
+import java.util.EventListener;
+import java.util.EventObject;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by wenshao on 2018/6/23.
+ * 自定义监听器中通过EventObject判断事件来源，所以前面说EventObject是起路由功能。
+ */
+
+public class MyEventListener implements EventListener {
+
+    public void onMessage(EventObject e, String args) {
+        System.out.println(e.getSource());
+        System.out.println(args);
+    }
+    public void onClientSync(EventObject e, String jsonString) {
+
+    }
+
+}
